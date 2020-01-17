e61cb71142344e515591d0c3e1434edfd3ce546b
kmcintosh <kmcintosh@groupon.com>
2020-01-16 17:16:52 -0800

    Smoothing out the format of the output

--- benchmark.sh
+++ benchmark.sh
@@ -11,19 +11,21 @@ docker rmi -f ${base_image}
 docker rmi -f node:12-alpine
 echo "--------------"
 
-echo "-------"
+echo "--------------"
 echo "Building with Base Image, first one will be the longest"
+echo "Docker image: ${isid}/docker_example_child:latest"
 for i in {5..1}
 do
   time ./build_base_child_image.sh > /dev/null
 done
-echo "-------"
+echo "--------------"
 
-echo "-------"
+echo "--------------"
 echo "Building with new image every time and no new caches"
+echo "Docker image: ${isid}/docker_base_all:latest"
 for i in {5..1}
 do
   docker rmi -f node:12-alpine > /dev/null
-  time docker build -t kmcintosh/docker_base_all -f ./Dockerfile_all . --no-cache=true > /dev/null
+  time docker build -t ${isid}/docker_base_all -f ./Dockerfile_all . --no-cache=true > /dev/null
 done
-echo "-------"
+echo "--------------"
